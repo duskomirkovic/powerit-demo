d9bd2a6af1726fa5f628a390d1e4eec3b0412ce5
Dusko Mirkovic <dusko.mirkovic@schneider-electric-dms.com>
2021-09-23 16:59:52 +0200

    Fixed ExampleFiles path so we can run from PerfView

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace Demo
             StartNewThread(() => Demonstrate("Parallel division-based (Functional)", ParallelPrimeNumbers.Create));
             StartNewThread(() => Demonstrate("Exception eating", ExceptionThrowingGenerator.Create));
             
-            long exampleDirSize = GetFileLengthsWHash(@".\ExampleFiles");
+            long exampleDirSize = GetFileLengthsWHash(@"C:\Workspace\PowerIT\ExampleFiles");
             Console.WriteLine("\nExample directory size is {0} bytes.\n", exampleDirSize);
         }
 
